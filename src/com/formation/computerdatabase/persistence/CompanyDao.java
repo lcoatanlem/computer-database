2fdfef2da2e58350d299e1086db80a8f3c436f3c
Lucas Coatanlem <lcoatanlem@excilys.com>
2016-03-08 10:21:37 +0100

    Coming back to spec-cdb version (computer-database may not be the right spec).

--- src/com/formation/computerdatabase/persistence/CompanyDao.java
+++
@@ -1,8 +0,0 @@
-package com.formation.computerdatabase.persistence;
-
-import com.formation.computerdatabase.model.Company;
-
-public interface CompanyDao extends Dao<Company> {
-	static final String KEY = "companyDao";
-
-}
