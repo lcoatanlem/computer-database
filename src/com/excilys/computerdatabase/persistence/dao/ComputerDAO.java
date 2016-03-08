fe9a0ea0f812c82d1bb365d560c64813001ab092
Lucas Coatanlem <lcoatanlem@excilys.com>
2016-03-08 18:59:44 +0100

    Adding every missing method (CRUD) for Computer DAO. Javadoc written for every single class in the project. Need to test it.

--- src/com/excilys/computerdatabase/persistence/dao/ComputerDAO.java
+++ src/com/excilys/computerdatabase/persistence/dao/ComputerDAO.java
@@ -11,9 +11,16 @@ import com.excilys.computerdatabase.model.Computer;
 import com.excilys.computerdatabase.persistence.mapping.ComputerMapping;
 import com.excilys.computerdatabase.persistence.mapping.Mapping;
 
+/**
+ * Class DAO for Computers, every method from interface DAO is implemented.
+ * @author lcoatanlem
+ */
 public class ComputerDAO implements DAO<Computer>{
 
 	@Override
+	/**
+	 * findAll returns a mapped Set<Computer>.
+	 */
 	public Set<Computer> findAll() {
 		Statement stmt;
 		ResultSet rs = null;
@@ -30,6 +37,9 @@ public class ComputerDAO implements DAO<Computer>{
 	}
 
 	@Override
+	/**
+	 * Creates a new line in the DB for the Computer t.
+	 */
 	public void create(Computer t) throws NotSuchCompanyException {
 		Statement stmt;
 		ResultSet rs = null;
@@ -50,6 +60,9 @@ public class ComputerDAO implements DAO<Computer>{
 	}
 
 	@Override
+	/**
+	 * Reads, and gives full information about a specific Computer (using id).
+	 */
 	public Computer read(Long id) throws NotSuchComputerException {
 		Statement stmt;
 		ResultSet rs = null;
@@ -66,6 +79,10 @@ public class ComputerDAO implements DAO<Computer>{
 	}
 
 	@Override
+	/**
+	 * Update a computer in the DB. If the id of the given Computer doesn't exists, raises
+	 * a NotSuchComputerException.
+	 */
 	public void update(Computer t) throws NotSuchComputerException {
 		Statement stmt;
 		ResultSet rs = null;
@@ -89,7 +106,25 @@ public class ComputerDAO implements DAO<Computer>{
 	}
 
 	@Override
+	/**
+	 * Delete a computer in the DB. If the id of the given Computer doesn't exists, raises
+	 * a NotSuchComputerException.
+	 */
 	public void delete(Computer t) throws NotSuchComputerException {
+		Statement stmt;
+		ResultSet rs = null;
+		try {
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery("SELECT * FROM computer WHERE id = " + t.getId() + ";");
+			if (rs.next()){
+				Statement stmt2 = conn.createStatement();
+				stmt2.executeUpdate("DELETE FROM computer WHERE id = " + t.getId() + ";");
+			} else {
+				throw new NotSuchComputerException();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	
