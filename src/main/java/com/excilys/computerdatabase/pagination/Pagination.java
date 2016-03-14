5e45199efd64c1f1a563d40ea437e5d0816c32b4
Lucas Coatanlem <lcoatanlem@excilys.com>
2016-03-14 15:53:54 +0100

    Pagination for Companies. Number of pages not implemented yet.

---
+++ src/main/java/com/excilys/computerdatabase/pagination/Pagination.java
@@ -0,0 +1,32 @@
+package main.java.com.excilys.computerdatabase.pagination;
+
+import java.util.List;
+
+import main.java.com.excilys.computerdatabase.model.Company;
+import main.java.com.excilys.computerdatabase.model.Computer;
+
+public class Pagination {
+	private Computer cpu;
+	private List<Computer> listeCpu;
+	private List<Company> listeCpn;
+	
+	public Computer getCpu() {
+		return cpu;
+	}
+	public void setCpu(Computer cpu) {
+		this.cpu = cpu;
+	}
+	public List<Computer> getListeCpu() {
+		return listeCpu;
+	}
+	public void setListeCpu(List<Computer> listeCpu) {
+		this.listeCpu = listeCpu;
+	}
+	public List<Company> getListeCpn() {
+		return listeCpn;
+	}
+	public void setListeCpn(List<Company> listeCpn) {
+		this.listeCpn = listeCpn;
+	}
+
+}
--- src/main/java/com/excilys/computerdatabase/service/CompanyController.java
+++ src/main/java/com/excilys/computerdatabase/service/CompanyController.java
@@ -1,21 +1,17 @@
 package main.java.com.excilys.computerdatabase.service;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import main.java.com.excilys.computerdatabase.model.Company;
+import main.java.com.excilys.computerdatabase.pagination.Pagination;
 import main.java.com.excilys.computerdatabase.persistence.dao.impl.CompanyDAOImpl;
 
 public class CompanyController {
 	private CompanyDAOImpl cDAO ;
-	private List<Company> liste;
 	
 	/**
 	 * Instantiates the DAO and the List containing all companies.
 	 */
 	public CompanyController(){
 		cDAO = new CompanyDAOImpl();
-		liste = new ArrayList<Company>();
 	}
 	
 	/**
@@ -26,21 +22,16 @@ public class CompanyController {
 	 * @throws IndexOutOfBoundsException
 	 * @return a String containing the pretty printing of the instances we want.
 	 */
-	public String listCompanies(int begin, int pagination) throws IndexOutOfBoundsException{
-		liste = cDAO.findAll(begin,pagination);
-		if(begin < 0){
+	public String listCompanies(int begin, int paging) throws IndexOutOfBoundsException{
+		Pagination page = new Pagination();
+		page.setListeCpn(cDAO.findAll(begin,paging));
+		if(page.getListeCpn().size() == 0){
 			throw new IndexOutOfBoundsException();
 		}
-		if(begin > liste.size()){
-			throw new IndexOutOfBoundsException();
-		}
-		if((begin+pagination)>liste.size()){
-			pagination = liste.size() - begin;
-		}
-		String res = "\tCompanies (" + (begin+1) + "-" + (begin+pagination) + "/" + liste.size() + ")\n"
+		String res = "\tCompanies (" + (begin+1) + "-" + (begin+(page.getListeCpn().size())) + ")\n"
 				+ "\t Id \t Name\n\n";
-		for (int i = begin; i < begin+pagination; i ++){
-			res += "\t " + liste.get(i).getId() + "\t " + liste.get(i).getName() + "\n"; 
+		for (Company elt : page.getListeCpn()){
+			res += "\t " + elt.getId() + "\t " + elt.getName() + "\n"; 
 		}
 		return res;	
 	}
