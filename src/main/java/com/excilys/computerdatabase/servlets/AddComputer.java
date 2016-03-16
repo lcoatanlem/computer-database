6fee5f9ff1424b8d1b289c860654e7c22c882187
Lucas Coatanlem <lcoatanlem@excilys.com>
2016-03-16 18:01:12 +0100

    Starting addComputer.

--- src/main/java/com/excilys/computerdatabase/servlets/AddComputer.java
+++ src/main/java/com/excilys/computerdatabase/servlets/AddComputer.java
@@ -6,16 +6,27 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.excilys.computerdatabase.exception.NotSuchCompanyException;
+import com.excilys.computerdatabase.persistence.dto.ComputerDTO;
+import com.excilys.computerdatabase.service.ComputerService;
+
 /**
  * Servlet implementation class AddComputer
  */
 public class AddComputer extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private ComputerService cpuServ = new ComputerService();
+	private ComputerDTO cDto;
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			cpuServ.createComputer(cDto);
+		} catch (NotSuchCompanyException e) {
+			e.printStackTrace();
+		}
 		this.getServletContext().getRequestDispatcher("/WEB-INF/views/addComputer.jsp").forward( request, response );
 	}
 
