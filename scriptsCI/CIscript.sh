dba3fb00b403b931c88a0b038d5151b896ae8bb6
Lucas Coatanlem <lcoatanlem@excilys.com>
2016-04-01 18:23:25 +0200

    Modified script. First of the name.

--- scriptsCI/CIscript.sh
+++ scriptsCI/CIscript.sh
@@ -1,1 +1,5 @@
-docker run helloworld
+docker network create dualnet
+docker run lcoatanlem/mysql --net=dualnet -e MYSQL_ROOT_PASSWORD=root --name=mysql -d
+docker run lcoatanlem/java-mvn --net=dualnet --name=java-mvn -d
+docker stop mysql
+docker rm mysql
