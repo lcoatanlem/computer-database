bece1d73c0bd2755a0dfd879ed07adedc806979d
Lucas Coatanlem <lcoatanlem@excilys.com>
2016-04-11 12:31:11 +0200

    Correcting indentation bugs and -it option

--- scriptsCI/CIscript.sh
+++ scriptsCI/CIscript.sh
@@ -11,11 +11,13 @@ if [ $? -eq 1 ]; then
   echo "UNKNOWN - /mysql does not exist."
   docker run --net=dualnet -e MYSQL_ROOT_PASSWORD=root --name=mysql -d lcoatanlem/mysql
 fi
+
 if [ "$MYSQLRUN" == "false" ]; then
   echo "CRITICAL - /mysql is not running."
   docker rm mysql
   docker run --net=dualnet -e MYSQL_ROOT_PASSWORD=root --name=mysql -d lcoatanlem/mysql
 fi
+
 if [ "$MYSQLRUN" == "true" ]; then
   echo "OK - /mysql is already running."
 fi
@@ -23,16 +25,18 @@ fi
 JAVAMVNRUN=$(docker inspect --format="{{ .State.Running }}" "java-mvn")
 if [ $? -eq 1 ]; then
   echo "UNKNOWN - /java-mvn does not exist."
-  docker run --net=dualnet --name=java-mvn -it lcoatanlem/java-mvn
+  docker run --net=dualnet --name=java-mvn -i lcoatanlem/java-mvn
 fi
+
 if [ "$MYSQLRUN" == "false" ]; then
   echo "CRITICAL - /java-mvn is not running."
   docker rm java-mvn
-  docker run --net=dualnet --name=java-mvn -it lcoatanlem/java-mvn
+  docker run --net=dualnet --name=java-mvn -i lcoatanlem/java-mvn
 fi
+
 if [ "$MYSQLRUN" == "true" ]; then
   echo "OK - /java-mvn is already running."
   docker stop java-mvn
   docker rm java-mvn
-  docker run --net=dualnet --name=java-mvn -it lcoatanlem/java-mvn
+  docker run --net=dualnet --name=java-mvn -i lcoatanlem/java-mvn
 fi
