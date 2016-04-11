42a4b1bdf85da965ccd6d1c6af8a9c942e26afc5
Lucas Coatanlem <lcoatanlem@excilys.com>
2016-04-11 11:02:52 +0200

    Bug in script.

--- scriptsCI/CIscript.sh
+++ scriptsCI/CIscript.sh
@@ -1,8 +1,5 @@
 docker network create dualnet
-docker run --net=dualnet -e MYSQL_ROOT_PASSWORD=root --name=mysql -d lcoatanlem/mysql 
-&& docker run --net=dualnet --name=java-mvn -it lcoatanlem/java-mvn 
-&& docker stop java-mvn 
-&&docker stop mysql
+docker run --net=dualnet -e MYSQL_ROOT_PASSWORD=root --name=mysql -d lcoatanlem/mysql && docker run --net=dualnet --name=java-mvn -it lcoatanlem/java-mvn && docker stop java-mvn && docker stop mysql
 docker rm java-mvn
 docker rm mysql
 docker network rm dualnet
