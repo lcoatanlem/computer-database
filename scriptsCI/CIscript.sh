80d0d11ab4e3386b9b8903ae699bdbb3e2d8f4de
Lucas Coatanlem <lcoatanlem@excilys.com>
2016-04-11 11:07:35 +0200

    Checking with wait.

--- scriptsCI/CIscript.sh
+++ scriptsCI/CIscript.sh
@@ -1,5 +1,8 @@
 docker network create dualnet
-docker run --net=dualnet -e MYSQL_ROOT_PASSWORD=root --name=mysql -d lcoatanlem/mysql && docker run --net=dualnet --name=java-mvn -it lcoatanlem/java-mvn && docker stop java-mvn && docker stop mysql
+docker run --net=dualnet -e MYSQL_ROOT_PASSWORD=root --name=mysql -d lcoatanlem/mysql && wait 1
+docker run --net=dualnet --name=java-mvn -it lcoatanlem/java-mvn && wait 1
+docker stop java-mvn
+docker stop mysql
 docker rm java-mvn
 docker rm mysql
 docker network rm dualnet
