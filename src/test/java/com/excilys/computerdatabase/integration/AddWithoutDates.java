4a75c9f203e792594d6ddb0c29943ff8d56f8c3f
Lucas Coatanlem <lcoatanlem@excilys.com>
2016-04-12 17:37:56 +0200

    Modified pom.xml to launch Tomcat for integration tests. Changed package name for integration tests. Put localhost for the database, as soon as we are back from docker.

---
+++ src/test/java/com/excilys/computerdatabase/integration/AddWithoutDates.java
@@ -0,0 +1,50 @@
+package com.excilys.computerdatabase.integration;
+
+import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class AddWithoutDates {
+  private WebDriver driver;
+  private String baseUrl;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  /**
+   * Launching firefox and the webapp.
+   * 
+   */
+  @Before
+  public void setUp() {
+    driver = new FirefoxDriver();
+    baseUrl = "http://localhost:8080/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testAddWithoutDates() throws InterruptedException {
+    driver.get(baseUrl + "/computer-database/dashboard");
+    driver.findElement(By.linkText("MacBook Pro 15.4 inch")).click();
+    driver.findElement(By.linkText("Cancel")).click();
+    driver.findElement(By.linkText("CM-5e")).click();
+    // asserts (message d'erreur si message erreur, page de retour correcte...)
+  }
+
+  /**
+   * Leave firefox after running all tests.
+   */
+  @After
+  public void tearDown() {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+}
